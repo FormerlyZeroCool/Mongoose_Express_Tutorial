2bbe3afd8e5d0861a2c416832fff5b9d5cdba12e
andrewrubinstein <andrew.rubinstein93@gmail.com>
2022-06-02 13:48:31 -0400

    Remove commented boilplate for https server

--- app.js
+++ app.js
@@ -3,19 +3,18 @@
     dotenv.config();
 //Include express classes
     const express = require('express');
-    const expBodyParser = require('body-parser');
-    const routerGen = require('./router');
 //Instantiate handlers for http and https
     const http = require('http');
 //express app instantiation
     const app = express();
     const httpServer = http.createServer(app);
+//require custom router implementation for db example app
+    const routerGen = require('./router');
 //Middleware Definition 
     app.use(express.json());
     routerGen.gen(app);
 //End Middleware definition
 
-
 //Start Server
-console.log("Starting up server on ports: "+process.env.PORT+", "+process.env.PORTSSL);
+console.log("Starting up server on ports: "+process.env.PORT);
 httpServer.listen(process.env.PORT,() => console.log('Server started listening on port: '+process.env.PORT));
