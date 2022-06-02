b7fe00327c7c41424075939ebc49f7b8a2bfdaaf
andrewrubinstein <andrew.rubinstein93@gmail.com>
2022-06-02 13:24:55 -0400

    Remove commented boilplate for https server

--- app.js
+++ app.js
@@ -5,19 +5,11 @@
     const express = require('express');
     const expBodyParser = require('body-parser');
     const routerGen = require('./router');
-
 //Instantiate handlers for http and https
-const http = require('http');
-const https = require('https');
-//Read Certificate from filesystem
-const fs = require('fs');
-//const privateKey = fs.readFileSync(process.env.KEY);
-//const certificate = fs.readFileSync(process.env.CERT);
-//const credentials = {key: privateKey, cert: certificate};
+    const http = require('http');
 //express app instantiation
     const app = express();
     const httpServer = http.createServer(app);
-    //const httpsServer = https.createServer(credentials, app);
 //Middleware Definition 
     app.use(express.json());
     routerGen.gen(app);
@@ -27,4 +19,3 @@ const fs = require('fs');
 //Start Server
 console.log("Starting up server on ports: "+process.env.PORT+", "+process.env.PORTSSL);
 httpServer.listen(process.env.PORT,() => console.log('Server started listening on port: '+process.env.PORT));
-//httpsServer.listen(process.env.PORTSSL,() => console.log('Server started listening on port: '+process.env.PORTSSL));
